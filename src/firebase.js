7ecf1ef47a3e755b521c43d1d87ccfaa96db1e6d
mushu <martial.tim@hotmail.com>
2025-10-05 20:24:00 +0200

    env changed

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 //import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSg3j8_KWWaJ8pW5PnEbgiM5cBMuSGDHI",
-  authDomain: "sourdough-mushubinks.firebaseapp.com",
-  projectId: "sourdough-mushubinks",
-  storageBucket: "sourdough-mushubinks.firebasestorage.app",
-  messagingSenderId: "541712514205",
-  appId: "1:541712514205:web:b1d5ec9fb886c214106874",
-  measurementId: "G-TDFSKX4L90"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
